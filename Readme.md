574b997e40558fde036f084baf42101f144566ed
xHondaa <mohanadahmd2002@gmail.com>
2023-02-28 11:03:15 +0200

    Main Files

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Internet checker is a simple script I made to either check the status of my internet and have it alert me when it comes back up or check the status of a specific site that's temporarily unavailabe
+
+You can open the main.py file with any text editor and you will find comments on specific parts you can change to suit your needs such as the method it asks for the delay between each time it checks and the site it checks from
+
+All you need to run this script is python and playsound library
+
+Use "pip install playsound==1.2.2" to download the library with the version that it works with
---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import urllib.request
+from playsound import playsound
+
+down = True
+sec = 1
+min = sec * 60
+hour = sec * 3600
+
+timeChosen = float(input("How often do you want me to check? (in seconds)\n"))
+
+#Uncomment this if you want to change the method with which you choose the delay between each request
+
+# timeChosen = int(input("How many hours do you want me to check?\n"))*hour + int(input("How many minutes?"))*min + int(input("How many seconds?\n"))*sec
+
+#The URL requested to check with
+#url = "https://eng.asu.edu.eg/"
+url = "https://www.google.com"
+
+while down:
+    t = timeChosen
+    try:
+        x = urllib.request.urlopen(url).reason
+        print(x)
+        down = False
+        playsound("a.mp3")
+    except urllib.error.URLError:
+        print("Still down")
+        print("Checking again in " + str(timeChosen) + " seconds.")
+        time.sleep(float(timeChosen))
+#        print('mmm')
